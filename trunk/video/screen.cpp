aaa7b0df54ab442d76a8b8fc1e8d7daa8a90e502
gabulor <gabulor@3b2cecd1-4895-fed2-517b-7f357f6139a6>
2011-04-19 03:32:14 +0000

    GUI

---
+++ trunk/video/screen.cpp
@@ -0,0 +1,182 @@
+#include "screen.h"
+#include "filter.h"
+
+#include <cutil_inline.h>
+#include <cutil_gl_error.h>
+#include <cuda_gl_interop.h>
+
+#include <QDateTime>
+#include <QTimer>
+#include <QDebug>
+
+Screen::Screen(QWidget *parent) :
+    QGLWidget(parent)
+{
+    PBO = 0;
+
+    timer = new QTimer(this);
+    timer->setInterval(30);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateTexture()));
+}
+
+void Screen::initializeGL()
+{
+    glewInit();
+
+    glClearColor(0.85f, 0.85f, 0.85f, 0.0f);
+    glEnable(GL_TEXTURE_2D);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+}
+
+void Screen::resizeGL(int w, int h)
+{
+    glViewport(0, 0, (GLint)w, (GLint)h);
+    ratio = (GLfloat)h/w;
+}
+
+void Screen::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    GLfloat r = (frame.data ? (GLfloat)frame.rows/frame.cols : ratio) / ratio;
+    GLfloat h = r < 1.0f ? r : 1.0f;
+    GLfloat w = r > 1.0f ? 1.0f/r : 1.0f;
+
+    glBegin(GL_TRIANGLE_FAN);
+        glTexCoord2f(0.0f, 0.0f); glVertex2f(-w,  h);
+        glTexCoord2f(1.0f, 0.0f); glVertex2f( w,  h);
+        glTexCoord2f(1.0f, 1.0f); glVertex2f( w, -h);
+        glTexCoord2f(0.0f, 1.0f); glVertex2f(-w, -h);
+    glEnd();
+
+    glFlush();
+}
+
+void Screen::runCUDA()
+{
+    Mat image;
+    cvtColor(frame, image, CV_BGR2RGBA);
+
+    void *buffer;
+    CUDA_SAFE_CALL(cudaGLMapBufferObject(&buffer, PBO));
+    medianFilter(image.data, (unsigned char*)buffer, image.cols, image.rows, 10);
+    //cudaMemcpy(buffer, image.data, image.step*image.rows, cudaMemcpyHostToDevice);
+    CUDA_SAFE_CALL(cudaGLUnmapBufferObject(PBO));
+
+    glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, PBO);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+    glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
+}
+
+void Screen::paintEvent(QPaintEvent *)
+{
+    updateGL();
+}
+
+void Screen::start()
+{
+    if (video.isOpened()) {
+        last_time = QDateTime::currentMSecsSinceEpoch();
+        timer->start();
+    }
+}
+
+void Screen::stop()
+{
+    if (video.isOpened()) {
+        timer->stop();
+    }
+}
+
+void Screen::createTexture()
+{
+    GLsizei size = frame.cols*frame.rows*4;
+
+    glGenBuffersARB(1, &PBO);
+    glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, PBO);
+    glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, size, NULL, GL_STREAM_READ);
+    glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
+
+    CUDA_SAFE_CALL(cudaGLRegisterBufferObject(PBO));
+
+    video_time = 0;
+
+    qDebug() << frame.cols << "x" << frame.rows;
+}
+
+void Screen::releaseTexture()
+{
+    if (PBO) {
+        CUDA_SAFE_CALL(cudaGLUnregisterBufferObject(PBO));
+        glDeleteBuffersARB(1, &PBO);
+        PBO = 0;
+    }
+}
+
+void Screen::updateTexture()
+{
+    runCUDA();
+    updateGL();
+    if (video.isOpened()) {
+        double current_time = QDateTime::currentMSecsSinceEpoch();
+        video_time += current_time-last_time;
+        last_time = current_time;
+        while (video_time > video.get(CV_CAP_PROP_POS_MSEC)) {
+            video >> frame;
+        }
+    }
+}
+
+void Screen::openVideo(QString path)
+{
+    releaseTexture();
+    release();
+    video.open(path.toStdString());
+    if (!video.isOpened()) {
+        qDebug() << "!video.isOpened()";
+        return;
+    }
+    video >> frame;
+    createTexture();
+    start();
+}
+
+void Screen::openPhoto(QString path)
+{
+    releaseTexture();
+    release();
+    frame = imread(path.toStdString());
+    createTexture();
+    updateTexture();
+}
+
+void Screen::captureCamera()
+{
+    releaseTexture();
+    release();
+    video.open(0);
+    if (!video.isOpened()) {
+        qDebug() << "!video.isOpened()";
+        return;
+    }
+    video >> frame;
+    createTexture();
+    start();
+}
+
+void Screen::release()
+{
+    if (video.isOpened()) {
+        video.release();
+    }
+    stop();
+}
---
+++ trunk/video/video.pro
@@ -0,0 +1,85 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-04-13T18:28:00
+#
+#-------------------------------------------------
+
+QT += core gui
+QT += opengl
+
+TARGET = video
+TEMPLATE = app
+
+SOURCES += \
+    main.cpp\
+    gui.cpp \
+    screen.cpp
+
+HEADERS += \
+    gui.h \
+    screen.h \
+    filter.h \
+    filter.h
+
+FORMS += \
+    gui.ui
+
+RESOURCES += \
+    resource.qrc
+
+#-------------------------------------------------
+#   CUDA
+#-------------------------------------------------
+
+CUDA_SOURCES = filter.cu
+
+win32 {
+  INCLUDEPATH += $(CUDA_INC_PATH) $(CUTIL_INC_PATH)
+  QMAKE_LIBDIR += $(CUDA_LIB_PATH) $(CUTIL_LIB_PATH)
+  LIBS += cudart.lib cutil32D.lib glew32.lib
+  QMAKE_LFLAGS += /NODEFAULTLIB:libcmt
+
+  cuda.output = $$OBJECTS_DIR${QMAKE_FILE_BASE}_cuda.obj
+  cuda.commands = $(CUDA_BIN_PATH)/nvcc.exe \
+    --machine 32 -ccbin \"$(VCINSTALLDIR)/bin\" -maxrregcount=16 --ptxas-options=-v --compile \
+    -Xcompiler \"/EHsc /W3 /nologo /O2 /Zi /MT\" $$join(INCLUDEPATH,'" -I "','-I "','"') ${QMAKE_FILE_NAME} -o ${QMAKE_FILE_OUT}
+}
+unix {
+  # auto-detect CUDA path
+  CUDA_DIR = $$system(which nvcc | sed 's,/bin/nvcc$,,')
+  INCLUDEPATH += $$CUDA_DIR/include
+  QMAKE_LIBDIR += $$CUDA_DIR/lib
+  LIBS += -lcudart
+
+  cuda.output = ${OBJECTS_DIR}${QMAKE_FILE_BASE}_cuda.obj
+  cuda.commands = nvcc -c -Xcompiler $$join(QMAKE_CXXFLAGS,",") $$join(INCLUDEPATH,'" -I "','-I "','"') ${QMAKE_FILE_NAME} -o ${QMAKE_FILE_OUT}
+  cuda.depends = nvcc -M -Xcompiler $$join(QMAKE_CXXFLAGS,",") $$join(INCLUDEPATH,'" -I "','-I "','"') ${QMAKE_FILE_NAME} | sed "s,^.*: ,," | sed "s,^ *,," | tr -d '\\\n'
+}
+cuda.input = CUDA_SOURCES
+QMAKE_EXTRA_COMPILERS += cuda
+
+#-------------------------------------------------
+#   OpenCV
+#-------------------------------------------------
+
+INCLUDEPATH += \
+    C:/OpenCV2.2/include
+
+QMAKE_LIBDIR += \
+    C:/OpenCV2.2/lib
+
+LIBS += \
+    opencv_calib3d220d.lib \
+    opencv_contrib220d.lib \
+    opencv_core220d.lib \
+    opencv_features2d220d.lib \
+    opencv_ffmpeg220d.lib \
+    opencv_flann220d.lib \
+    opencv_gpu220d.lib \
+    opencv_highgui220d.lib \
+    opencv_imgproc220d.lib \
+    opencv_legacy220d.lib \
+    opencv_ml220d.lib \
+    opencv_objdetect220d.lib \
+    opencv_ts220.lib \
+    opencv_video220d.lib
